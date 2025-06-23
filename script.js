bfaa68cb5a0310701f97d76c033217b4b34a61f7
0fick <tati141098@gmail.com>
2025-06-23 23:32:00 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+function openInvitation() {
+  document.querySelector('.cover').style.display = 'none';
+  document.querySelector('.content').classList.remove('hidden');
+  document.getElementById('music').play();
+}
